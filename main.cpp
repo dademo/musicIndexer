2bd7d6a75400f694761533507b0567a5b128fb52
Damien DEMONAZ <damien.demonaz@gmail.com>
2017-08-05 00:17:14 +0200

    Testing the new class : TagInfo

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "sqlite3.h"
 #include "music_sqlTables.hpp"
 #include "fileManip.hpp"
+#include "tagfcts.hpp"
 
 #define DBNAME "music.db"
 
@@ -17,41 +18,28 @@ int main (int argc, char* argv[])
 	if( rc ){
 		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
 		sqlite3_close(db);
-		return(1);
+		return 1;
 	}
-	else { std::cout << "rc = " << rc << std::endl; }
-
-	// Creating the database if not exist
-
-	/*char request[] = "CREATE TABLE IF NOT EXISTS testTable (id INTEGER PRIMARY KEY AUTOINCREMENT, data TEXT)";
-	sqlite3_stmt* requestStatement;
+	else
+	{
 	
-	int returnVal = sqlite3_prepare_v2(db,(const char*) request, -1, &requestStatement, (const char**)  &err);
-	if(returnVal == SQLITE_OK)
-	{
-		returnVal = sqlite3_step(requestStatement);
-		if(returnVal == SQLITE_DONE)		{ std::cout << " Request OK (SQLITE_DONE) !" << std::endl; }
-		else if (returnVal == SQLITE_ROW)	{ std::cout << " Request OK (SQLITE_ROW) !" << std::endl; }
-		else { std::cout << "Error(2) : " << returnVal << std::endl; }
+		checkTables(db);
+
+		std::vector<std::string> fileList = getFileList("/home/dademo/Musique", false);
+
+		//TagInfos maMusique("/home/dademo/Musique/Flac/Daft Punk/Musique Vol. 1 (1993-2005)/1 - Daft Punk - Musique.flac");
+		TagInfos maMusique("/home/dademo/Musique/Flac/Daft Punk/Random Access Memories/1 - Daft Punk - Give Life Back to Music.flac");
+	/*
+		for(std::vector<std::string>::iterator it = fileList.begin(); it != fileList.end(); it++)
+		{
+			std::cout << *it << std::endl;
+		}
+	*/
+
+		std::cout << maMusique.toString() << std::endl;
+
+		sqlite3_close(db);
+
+		return 0;
 	}
-	else { std::cout << "Error(1) : " << returnVal << std::endl << "Error : " << err << std::endl;; }
-	if( sqlite3_finalize(requestStatement) == SQLITE_OK) { std::cout << "Finalize : OK !" << std::endl; }*/
-
-	//std::cout << "Hello world !" << std::endl;
-
-	//createTables(db);
-
-	checkTables(db);
-
-	std::vector<std::string> fileList = getFileList("/home/dademo/Musique", false);
-
-	for(std::vector<std::string>::iterator it = fileList.begin(); it != fileList.end(); it++)
-	{
-		std::cout << *it << std::endl;
-	}
-
-
-	sqlite3_close(db);
-
-	return 0;
 }
