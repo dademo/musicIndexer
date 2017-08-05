dde19f8455ce6c19c78223885263a458b935a8a0
Damien DEMONAZ <damien.demonaz@gmail.com>
2017-08-06 01:20:48 +0200

    Added function idSearch

--- mySQLiteFunctions.hpp
+++ mySQLiteFunctions.hpp
@@ -1,6 +1,11 @@
 #ifndef MYSQLITEFUNCTIONS
 #define MYSQLITEFUNCTIONS
 
+#include "sqlite3.h"
+#include <string>
+
 int sqliteReturnVal(int returnVal, const char* err);
+
+int idResearch(sqlite3* db, std::string dbName, std::string paramName, std::string paramValue);
 
 #endif
