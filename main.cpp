cbd4abf84a7e7fa619879ad46212a4c53020649d
Damien DEMONAZ <damien.demonaz@gmail.com>
2017-08-27 16:27:01 +0200

    Deleted some code of main.cpp -> fct_utiles && tagfcts

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <map>
 #include <vector>
 #include <stdlib.h>	// atoi()
+
+// TEMP //
+#include <unistd.h>
 
 #include "sqlite3.h"
 #include "music_sqlTables.hpp"
 #include "fileManip.hpp"
+#include "fct_utiles.hpp"
 #include "tagfcts.hpp"
+#include "class_tagInfos.hpp"
 
 #define DBNAME "/tmp/music.db"
 
@@ -29,38 +35,18 @@ int main (int argc, char* argv[])
 	else
 	{
 	
-		std::string supportedExtensions[18] = {"mp3", "ogg", "spx", "mpc", "ape", "flac", "wv", "tta", "wma", "m4a", "m4b", "m4p", "mp4", "3g2", "wav", "aif", "aiff", "opus"};
-
 		//TagInfos maMusique("/home/dademo/Musique/Playlist Wave.m3u");
 
 		checkTables(db);
 
-		std::vector<std::string> fileList = getFileList(origPath, false);
-		std::vector<TagInfos> allMyTags;
 
-		for(std::vector<std::string>::iterator it = fileList.begin(); it != fileList.end(); it++)
-		{
-			std::string extension = getFileLastExtension(*it);
-			bool ok = false;
-			for(int i = 0; i < 18; i++)
-			{
-				if(extension == supportedExtensions[i]) { ok = true; break; }
-				/*if(extension == supportedExtensions[i]) {
-					if((origPath + "/" + *it).find("Daft") != std::string::npos)
-					{ ok = true; break; } }*/
-			}
-			if(ok)
-			{
-				std::string temp = *it;
-				std::cout << origPath + "/" + temp << std::endl;
-				allMyTags.push_back(TagInfos(origPath + "/" + temp));
-			}
-		}
+		//std::vector<TagInfos> allMyTags = fullGetAllTags(origPath);
+		std::vector<TagInfos> allMyTags = fastGetAllTags(origPath);
 		
 		for(std::vector<TagInfos>::iterator it = allMyTags.begin(); it != allMyTags.end(); it++)
 		{
 			//std::cout << it->toString() << std::endl;
-			std::cout << "Sycing : " << it->getData().name << " [" << it->getData().album.name << "]" << std::endl;
+			std::cout << "Sycing : "  << " [" << it->getData().album.name << "]\t" << it->getData().name<< std::endl;
 			it->sync(db);
 		}
 
@@ -79,24 +65,13 @@ int main (int argc, char* argv[])
 			{
 				std::cout << ((i%2)? "-":"_");
 			}*/
-			std::cout << std::endl;
+			//std::cout << std::endl;
 		}
 
 		//TagInfos(db, 2).delDataFromDb(db);
 		/*struct songInfos data;
 
 		TagInfos::getSongInfosById(db, 1, &data);*/
-
-		//TagInfos maMusique("/home/dademo/Musique/Flac/Daft Punk/Musique Vol. 1 (1993-2005)/1 - Daft Punk - Musique.flac");
-		//TagInfos maMusique("/home/dademo/Musique/Flac/Daft Punk/Random Access Memories/1 - Daft Punk - Give Life Back to Music.flac");
-	/*
-		for(std::vector<std::string>::iterator it = fileList.begin(); it != fileList.end(); it++)
-		{
-			std::cout << *it << std::endl;
-		}
-	*/
-
-		//std::cout << maMusique.toString() << std::endl;
 
 
 		//maMusique.sync(db);
