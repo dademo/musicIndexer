a55ff2a070d4db52549adf1711087e4ee20223c0
Damien DEMONAZ <damien.demonaz@gmail.com>
2017-08-28 00:02:29 +0200

    Added 2 functions to generate void structures of the class_tagInfos.hpp file

--- fct_utiles.cpp
+++ fct_utiles.cpp
@@ -11,3 +11,32 @@ void clearLine(int nChar)
 	}
 }
 
+struct songInfos genVoidStructSongInfos()
+{
+	struct songInfos toReturn;
+
+	toReturn.name="";
+		toReturn.album.name="";
+		toReturn.album.albumArtist="";
+		toReturn.album.nTracks=0;
+		toReturn.album.year="";
+	toReturn.artist="";
+	toReturn.tracknbr=0;
+	toReturn.genre="";
+	toReturn.comment="";
+	toReturn.fullPath="";
+
+	return toReturn;
+}
+
+struct audioProperties genVoidStructAudioProperties()
+{
+	struct audioProperties toReturn;
+
+	toReturn.length=0;
+	toReturn.bitrate=0;
+	toReturn.samplerate=0;
+	toReturn.channels=0;
+
+	return toReturn;
+}
--- fct_utiles.hpp
+++ fct_utiles.hpp
@@ -5,6 +5,12 @@
 #include <vector>
 #include <map>
 
+#include "class_tagInfos.hpp"
+
 void clearLine(int nChar);	// When using the '\r', to delete all previous characters
+
+struct songInfos genVoidStructSongInfos();
+struct audioProperties genVoidStructAudioProperties();
+
 
 #endif
