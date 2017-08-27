55a727f1a3b7475a1abf8557043edf39973ee26d
Damien DEMONAZ <damien.demonaz@gmail.com>
2017-08-27 16:34:23 +0200

    Added function getFileList -> Listing all files of a given path (used in fastGetAllTags and fullGetAllTags)

--- fileManip.cpp
+++ fileManip.cpp
@@ -24,7 +24,17 @@ std::vector<std::string> getFileList(std::string path, bool recursive)
 			//std::cout << ( (isDir(path + "/" + currentFile->d_name))? "Is a directory" : "Not a directory" ) << std::endl;
 			if(std::string(currentFile->d_name) != "." && std::string(currentFile->d_name) != "..")
 			{
-				if(!isDir(path + "/" + currentFile->d_name))	{ toReturn.push_back(currentFile->d_name); }
+				if(!isDir(path + "/" + currentFile->d_name))
+				{
+					bool ok = false;
+					std::string extension = getFileLastExtension(currentFile->d_name);
+					for(int i=0; i < 18; i++)
+					{
+						if(extension == supportedExtensions[i])
+						{ ok = true; break; }
+					}
+					if(ok) {toReturn.push_back(currentFile->d_name); }
+				}
 				else { dirList.push_back(currentFile->d_name); }
 			}
 		}
@@ -43,6 +53,24 @@ std::vector<std::string> getFileList(std::string path, bool recursive)
 		//toReturn.push_back(val);
 
 	}
+
+	return toReturn;
+}
+
+std::map<std::string, std::vector<std::string>> mapFileList(std::vector<std::string> fileList)
+{
+	std::map<std::string,std::vector<std::string>> toReturn;
+
+	for(std::vector<std::string>::iterator it = fileList.begin(); it != fileList.end(); it++)
+	{
+		std::string::size_type tmpPos = (*it).rfind("/");
+		std::string fileDir = (*it).substr(0, (tmpPos > (*it).length())? 0 : tmpPos);
+		toReturn[fileDir].push_back(
+			(*it).substr( (tmpPos > (*it).length() )? 0 : tmpPos+1, std::string::npos)	// +1 to ignore the "/"
+					);
+	}
+
+	//temp.substr(0,(pos > temp.length())? 0:pos)
 
 	return toReturn;
 }
--- fileManip.hpp
+++ fileManip.hpp
@@ -2,9 +2,13 @@
 #define FILEMANIP_HPP
 
 #include <vector>
+#include <map>
 #include <string>
 
+const std::string supportedExtensions[18] = {"mp3", "ogg", "spx", "mpc", "ape", "flac", "wv", "tta", "wma", "m4a", "m4b", "m4p", "mp4", "3g2", "wav", "aif", "aiff", "opus"};
+
 std::vector<std::string> getFileList(std::string dirPath, bool recursive);
+std::map<std::string, std::vector<std::string>> mapFileList(std::vector<std::string> fileList);
 bool fexists(std::string path);
 bool isDir(std::string path);
 std::string getFileLastExtension(std::string file);
