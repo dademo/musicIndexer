c484998ee72a542384739d4c6cfdb48850b5ac57
Damien DEMONAZ <damien.demonaz@gmail.com>
2017-09-10 15:40:33 +0200

    Added functions to make asynchrone syncs (require creating a function, else the std::thread calls the ASyncTagInfos::Sync function -> infinite loop)

--- class_asyncTagInfos.cpp
+++ class_asyncTagInfos.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include "class_asyncTagInfos.hpp"
 
+void testfct(int j)
+{
+	for(int i(0); i < j; i++)
+	{
+		std::cout << i << std::endl;
+	}
+}
+
 ASyncTagInfos::ASyncTagInfos(TagLib::FileRef targetFile) : TagInfos(targetFile), m_currentThread(NULL)
+{
+
+}
+
+ASyncTagInfos::ASyncTagInfos(std::string path) : TagInfos(TagLib::FileRef(path.c_str(), TagLib::AudioProperties::Average)), m_currentThread(NULL)
 {
 
 }
@@ -16,7 +29,50 @@ ASyncTagInfos::ASyncTagInfos(struct songInfos songTagInfos, struct audioProperti
 
 }
 
+ASyncTagInfos::ASyncTagInfos(const TagInfos& orig) : TagInfos(orig), m_currentThread(NULL)
+{
+
+}
+
+
 ASyncTagInfos::~ASyncTagInfos()
 {
-	if(m_currentThread == NULL) { delete m_currentThread; }
+	if(m_currentThread != NULL)	// Thread has not be joined
+	{
+		m_currentThread->join();
+		delete m_currentThread;
+	}
+}
+
+
+bool ASyncTagInfos::sync(sqlite3* db)
+{
+	if(m_currentThread == NULL)
+	{
+		try {
+			//m_currentThread = new std::thread(&TagInfos::sync, this, db);
+			m_currentThread = new std::thread(&ASyncTagInfos::TagInfos_sync, this, db);
+		} catch (std::system_error e) {
+			std::cerr << e.what() << std::endl;
+		}
+		return true;
+	} else { return false; }
+}
+
+bool ASyncTagInfos::TagInfos_sync(sqlite3* db)
+{
+	return TagInfos::sync(db);
+}
+
+void ASyncTagInfos::join()
+{
+	if(m_currentThread != NULL)
+	{
+		try {
+			m_currentThread->join();
+		} catch (std::system_error e) {
+			std::cerr << "join" << e.what() << std::endl;
+		}
+		delete m_currentThread; m_currentThread = NULL;
+	}
 }
--- class_asyncTagInfos.hpp
+++ class_asyncTagInfos.hpp
@@ -1,6 +1,7 @@
 #ifndef CLASS_ASYNCTAGINFOS_HPP
 #define CLASS_ASYNCTAGINFOS_HPP
 
+//#include <future>
 #include <thread>
 #include "class_tagInfos.hpp"
 
@@ -8,9 +9,10 @@ class ASyncTagInfos : public TagInfos
 {
 public:
 	ASyncTagInfos(TagLib::FileRef targetFile);
-	ASyncTagInfos(std::string path) : TagInfos(TagLib::FileRef(path.c_str(), TagLib::AudioProperties::Average)) {}
+	ASyncTagInfos(std::string path);
 	ASyncTagInfos(sqlite3* db, int songDbId);	// Creating an object from the DB, with only the song ID
 	ASyncTagInfos(struct songInfos songTagInfos, struct audioProperties songAudioProperties);
+	ASyncTagInfos(const TagInfos& orig);
 	virtual ~ASyncTagInfos();
 
 	virtual bool sync(sqlite3* db);
@@ -19,6 +21,8 @@ public:
 	void join();
 
 private:
+	bool TagInfos_sync(sqlite3* db);	// Function that call TagInfos::sync()
+	//std::future<bool> m_currentAction;
 	std::thread* m_currentThread;
 };
 
